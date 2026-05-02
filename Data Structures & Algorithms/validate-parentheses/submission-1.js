d3759b4335b5a7df46baa5f9c3e771d98dec656a
Bartolomeo Caruso <48251346+Krytons@users.noreply.github.com>
2026-05-02 08:27:16 +0200

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let operationStack = [];
+
+        for(let i=0; i < s.length; i++){
+            let currentChar = s[i];
+            let currentTop = operationStack[operationStack.length - 1];
+            switch(currentChar){
+                case '{':
+                case '[':
+                case '(':
+                    operationStack.push(currentChar);
+                    break;
+                case '}':
+                    if(!currentTop || currentTop !== '{')
+                        return false; 
+                    else 
+                        operationStack.pop();
+                    break;
+                case ']':
+                    if(!currentTop || currentTop !== '[')
+                        return false; 
+                    else 
+                        operationStack.pop();
+                    break;
+                case ')':
+                    if(!currentTop || currentTop !== '(')
+                        return false; 
+                    else 
+                        operationStack.pop();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return operationStack.length < 1;
+    }
+}

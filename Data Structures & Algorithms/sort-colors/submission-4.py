787c702ef1da8fb2b2618ae1292abd8c54001dbe
Bartolomeo Caruso <48251346+Krytons@users.noreply.github.com>
2026-05-11 08:09:46 +0200

    Add: sort-colors - submission-4

---
+++ Data Structures & Algorithms/sort-colors/submission-4.py
@@ -0,0 +1,21 @@
+def swapPosition(array, origin, destination):
+    array[origin], array[destination] = array[destination], array[origin]
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        lo = 0
+        hi = len(nums) - 1
+        mid = 0
+
+        while mid <= hi:
+            match nums[mid]:
+                case 0:
+                    if lo != mid:
+                        swapPosition(nums, mid, lo)
+                    lo += 1
+                    mid += 1
+                case 1: 
+                    mid += 1
+                case 2:
+                    swapPosition(nums, mid, hi)
+                    hi -= 1

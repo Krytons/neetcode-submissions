18c01d65c6390fe1d0e679b091fbb30f128f1ce7
Bartolomeo Caruso <48251346+Krytons@users.noreply.github.com>
2026-07-10 16:45:01 +0200

    Add: target-sum - submission-8

---
+++ Data Structures & Algorithms/target-sum/submission-8.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        currentSum = 0
+        return self.backtracking(0, nums, target, currentSum)
+
+    def backtracking(self, index, nums, target, currentSum):
+        if index == len(nums):
+            if currentSum == target:
+                return 1
+            return 0
+
+        #Choose the addition
+        results = 0
+        currentSum += nums[index]
+        results += self.backtracking(index + 1, nums, target, currentSum)
+        #Choose the subtraction
+        currentSum -= (nums[index] * 2) #The multiply is done to undo the summation
+        results += self.backtracking(index + 1, nums, target, currentSum)
+
+        return results

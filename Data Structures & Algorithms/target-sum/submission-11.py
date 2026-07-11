a5111c2e6dbbff8905e24d50f9434fab29e5b071
Bartolomeo Caruso <48251346+Krytons@users.noreply.github.com>
2026-07-11 10:52:39 +0200

    Add: target-sum - submission-11

---
+++ Data Structures & Algorithms/target-sum/submission-11.py
@@ -0,0 +1,46 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        # 1DP Collapse Solution
+        mapping = defaultdict(int)
+        mapping[0] = 1
+
+        for index in range(0, len(nums)):
+            nextMapping = defaultdict(int)
+            for summation in mapping:
+                nextMapping[summation + nums[index]] += mapping[summation] 
+                nextMapping[summation - nums[index]] += mapping[summation] 
+            mapping = nextMapping
+
+        return mapping[target]
+
+        """ 2DP Solution
+        mappings = [defaultdict(int) for element in range(len(nums)+1)]
+        mappings[0][0] = 1 
+
+        for index in range(0, len(nums)):
+            for summation, ways in mappings[index].items():
+                mappings[index + 1][summation + nums[index]] += ways 
+                mappings[index + 1][summation - nums[index]] += ways
+
+        return mappings[len(nums)][target]
+        """
+
+
+        """ Backtraging + Memoize Solution
+        currentSum = 0
+        cache = {}
+    
+        def backtracking(index, nums, target, currentSum):
+            if index == len(nums):
+                return 1 if currentSum == target else 0
+            elif (index, currentSum) in cache:
+                return cache[(index, currentSum)]
+
+
+            #Choose the addition
+            results = backtracking(index + 1, nums, target, currentSum + nums[index]) + backtracking(index + 1, nums, target, currentSum - nums[index])
+            cache[(index, currentSum)] = results
+            return results
+
+        return backtracking(0, nums, target, currentSum)
+        """
